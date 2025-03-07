d33fc5ade80b6850444b025751c1d070bdd20ffd
janeskil1525 <janeskil1525@gmail.com>
2025-03-07 16:33:15 +0100

    First Commit

--- lib/Daje/Plugin/Login.pm
+++ lib/Daje/Plugin/Login.pm
@@ -1,39 +1,51 @@
 package Daje::Plugin::Login;
-use 5.008001;
-use strict;
-use warnings;
+use Mojo::Base 'Mojolicious::Plugin', -signatures;
+use v5.40;
+
+# NAME
+# ====
+#
+# Daje::Plugin::Login - It's new $module
+#
+# SYNOPSIS
+# ========
+#
+#     use Daje::Plugin::Login;
+#
+# DESCRIPTION
+# ===========
+#
+# Daje::Plugin::Login is ...
+#
+# LICENSE
+# =======
+#
+# Copyright (C) janeskil1525.
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+#
+# AUTHOR
+# ======
+#
+# janeskil1525 E<lt>janeskil1525@gmail.comE<gt>
+#
 
 our $VERSION = "0.01";
 
 
+sub register ($self, $app, $config) {
+
+    my $r = $app->routes();
+    $r->put('/api/login/')->to('login#login_user');
+
+    $app->helper(login => sub {$login});
+
+    $app->log->debug("Daje::Plugin::Workflow registered");
+}
 
 1;
 __END__
 
-=encoding utf-8
 
-=head1 NAME
-
-Daje::Plugin::Login - It's new $module
-
-=head1 SYNOPSIS
-
-    use Daje::Plugin::Login;
-
-=head1 DESCRIPTION
-
-Daje::Plugin::Login is ...
-
-=head1 LICENSE
-
-Copyright (C) janeskil1525.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 AUTHOR
-
-janeskil1525 E<lt>janeskil1525@gmail.comE<gt>
-
-=cut
 
